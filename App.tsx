004e3d0f73f4e8cfe26c6a9cf5be99e851f16989
inoue, masatoshi <114462151+inomasa1013@users.noreply.github.com>
2023-06-29 15:29:45 +0900

    reactnativesetting
    Co-authored-by: inomasa1013 <inomasa1013@users.noreply.github.com>

--- App.tsx
+++ App.tsx
@@ -1,31 +1,31 @@
+import { StatusBar } from "expo-status-bar";
+import { Dimensions, StyleSheet, Text, TextInput, View } from "react-native";
+import Icon from "react-native-vector-icons/Ionicons";
+import { useState } from "react";
+import { withAuthenticator } from "aws-amplify-react-native";
 
-import { StatusBar } from 'expo-status-bar';
-import { Dimensions, StyleSheet, Text, TextInput, View } from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons';
-import { useState } from 'react';
+import Footer from "./components/Footer";
+import TinderSwipe from "./components/TinderSwipe";
 
-import Footer from './components/Footer';
-import TinderSwipe from './components/TinderSwipe';
+import Appp from "./components/favorite/App";
+import Apppp from "./components/favorite/spots/App";
 
-import Appp from './components/favorite/App';
-import Apppp from './components/favorite/spots/App';
-
-import { cards } from './data/cards';
+import { cards } from "./data/cards";
 
 const App = () => {
-
   const [page, setPage] = useState("home");
 
   return (
     <View style={styles.container}>
-
-      {page === "home" &&
+      {page === "home" && (
         <View>
           <View style={styles.header}>
             <Icon name="search-outline" style={styles.headerIcon} />
-            <TextInput placeholder="キーワード検索" style={styles.headerTextInput} />
+            <TextInput
+              placeholder="キーワード検索"
+              style={styles.headerTextInput}
+            />
             <Icon name="menu-outline" style={styles.headerIcon} />
-
           </View>
           <View style={styles.main}>
             <Text style={styles.mainText}>おすすめ終了！</Text>
@@ -34,11 +34,9 @@ const App = () => {
             ))}
           </View>
         </View>
-      }
+      )}
 
-      {page === "favorite" &&
-        <Appp />
-      }
+      {page === "favorite" && <Appp />}
 
       <Footer setPage={setPage} />
 
@@ -50,18 +48,18 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
+    backgroundColor: "white",
   },
   header: {
-    flexDirection: 'row',
-    backgroundColor: 'white',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    backgroundColor: "white",
+    justifyContent: "space-between",
     margin: 10,
     padding: 10,
     top: 50,
     // zIndex: 2,
     borderRadius: 30,
-    shadowColor: 'rgb(200, 200, 200)',
+    shadowColor: "rgb(200, 200, 200)",
     shadowOffset: {
       width: 0,
       height: 1,
@@ -70,19 +68,19 @@ const styles = StyleSheet.create({
   },
   headerIcon: {
     fontSize: 30,
-    color: 'rgb(130, 130, 130)',
+    color: "rgb(130, 130, 130)",
   },
   headerTextInput: {
-    width: '70%',
+    width: "70%",
   },
   main: {
-    position: 'absolute',
+    position: "absolute",
   },
   mainText: {
-    position: 'absolute',
-    marginTop: Dimensions.get('window').height / 2,
+    position: "absolute",
+    marginTop: Dimensions.get("window").height / 2,
     marginHorizontal: 150,
   },
 });
 
-export default App;
+export default withAuthenticator(App);
