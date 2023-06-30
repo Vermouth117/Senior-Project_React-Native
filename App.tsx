013fb1287bb35fdb2d4a46c927e9e49312f66b07
haya-sho <shoko_hasegawa@mail.toyota.co.jp>
2023-06-30 14:04:55 +0900

    footer change coloring

--- App.tsx
+++ App.tsx
@@ -1,30 +1,30 @@
+import { StatusBar } from "expo-status-bar";
+import { memo, useState } from "react";
+import { Dimensions, StyleSheet, Text, TextInput, View } from "react-native";
+import Icon from "react-native-vector-icons/Ionicons";
 
-import { StatusBar } from 'expo-status-bar';
-import { memo, useState } from 'react';
-import { Dimensions, StyleSheet, Text, TextInput, View } from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons';
+import TinderSwipe from "./components/home/TinderSwipe";
+import Favorites from "./components/favorites/Page";
+import Footer from "./components/Footer";
+import { cards } from "./data/cards";
+import Map from "./components/map/Map";
 
-import TinderSwipe from './components/home/TinderSwipe';
-import Favorites from './components/favorites/Page';
-import Footer from './components/Footer';
-import { cards } from './data/cards';
-import Map from './components/map/Map';
-
-const SCREEN_HEIGHT = Dimensions.get('window').height;
-const SCREEN_WIDTH = Dimensions.get('window').width;
+const SCREEN_HEIGHT = Dimensions.get("window").height;
+const SCREEN_WIDTH = Dimensions.get("window").width;
 
 const App = () => {
-
   const [page, setPage] = useState("home");
 
   return (
     <View style={styles.container}>
-
-      {page === "home" &&
+      {page === "home" && (
         <View>
           <View style={styles.header}>
             <Icon name="search-outline" style={styles.headerIcon} />
-            <TextInput placeholder="キーワード検索" style={styles.headerTextInput} />
+            <TextInput
+              placeholder="キーワード検索"
+              style={styles.headerTextInput}
+            />
             <Icon name="menu-outline" style={styles.headerIcon} />
           </View>
           <View style={styles.main}>
@@ -34,15 +34,11 @@ const App = () => {
             ))}
           </View>
         </View>
-      }
+      )}
 
-      {page === "notice" &&
-        <Map />
-      }
+      {page === "map" && <Map />}
 
-      {page === "favorites" &&
-        <Favorites />
-      }
+      {page === "favorites" && <Favorites />}
 
       <Footer setPage={setPage} />
 
@@ -54,17 +50,17 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
-    backgroundColor: 'white',
+    backgroundColor: "white",
   },
   header: {
-    flexDirection: 'row',
-    backgroundColor: 'white',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    backgroundColor: "white",
+    justifyContent: "space-between",
     margin: 10,
     padding: 10,
     top: 50,
     borderRadius: 30,
-    shadowColor: 'rgb(200, 200, 200)',
+    shadowColor: "rgb(200, 200, 200)",
     shadowOffset: {
       width: 0,
       height: 1,
@@ -73,20 +69,20 @@ const styles = StyleSheet.create({
   },
   headerIcon: {
     fontSize: 30,
-    color: 'rgb(130, 130, 130)',
+    color: "rgb(130, 130, 130)",
   },
   headerTextInput: {
-    width: '70%',
+    width: "70%",
   },
   main: {
-    position: 'absolute',
+    position: "absolute",
     height: SCREEN_HEIGHT + 30,
     width: SCREEN_WIDTH,
     paddingBottom: 100,
   },
   mainText: {
-    position: 'absolute',
-    marginTop: Dimensions.get('window').height / 2,
+    position: "absolute",
+    marginTop: Dimensions.get("window").height / 2,
     marginHorizontal: 150,
   },
 });
